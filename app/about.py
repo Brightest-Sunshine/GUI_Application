e5d7301307f1820df01069906df42b5a164170c4
Dmitroved <dmitroved@gmail.com>
2021-04-26 18:11:00 +0300

    add apps corpse

---
+++ app/about.py
@@ -0,0 +1,20 @@
+from PyQt5 import uic, QtWidgets, QtGui
+from app import UI_about
+import sys
+
+
+class AboutView(QtWidgets.QWidget, UI_about.Ui_About):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        self.pushButton.clicked.connect(self.ok_pressed)
+
+    def ok_pressed(self):
+        self.close()
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)  # Новый экземпляр QApplication
+    window = AboutView()  # Создаём объект класса ExampleApp
+    window.show()  # Показываем окно
+    sys.exit(app.exec_())  # и
